6899440a7e0b819110d9242b6795d069c35b8318
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2009-08-28 01:57:11 +0000

    - Shortened package name to "LSPM" - Added myself to Author/Maintainer - Added GPL notice to top of all ".R" files

--- R/lsp.optimize.R
+++ R/lsp.optimize.R
@@ -1,3 +1,22 @@
+#
+#   LSPM: The Leverage Space Portfolio Modeler
+#
+#   Copyright (C) 2009  Soren MacBeth, Joshua Ulrich, and Ralph Vince
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 lsp.optimize <- function(jpt, DEcontrol=list(), MAXCYCLES=18, sigmas=5.0, error.size=0.01, use.drawdown=TRUE, acceptable.percent=0.2, horizon=30, b=0.8, max.probprofit=TRUE, target.return=0.02,zminusmin=-1,zplusmin=-1) {
 	RRDR <- .jnew("com/leveragespacemodel/RRDRTestRaw",as.integer(MAXCYCLES),sigmas,error.size,use.drawdown,acceptable.percent,as.integer(horizon),b)
 	RRDRPP <- .jnew("com/leveragespacemodel/RRDRPPTestRaw",as.integer(MAXCYCLES),sigmas,error.size,use.drawdown,acceptable.percent,as.integer(horizon),b,target.return)
